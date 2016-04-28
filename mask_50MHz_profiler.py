ae1e915a54a62c54918b2db93a4744a4376d218e
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2016-04-28 16:40:54 -0600

    Latest.

--- mask_50MHz_profiler.py
+++ mask_50MHz_profiler.py
@@ -49,11 +49,11 @@ sf = nc.Dataset(flist[0])
 mf = nc.MFDataset(flist, aggdim='time')
 
 # Store data from the single file
-bf = sf.variables['bad_flag']
-palt = sf.variables['prof_site_altitude']
-plat = sf.variables['prof_site_latitude']
-plon = sf.variables['prof_site_longitude']
-pagl = sf.variables['prof_height_100m_AGL']
+bf = sf.variables['bad_flag'][:]
+palt = sf.variables['prof_site_altitude'][:]
+plat = sf.variables['prof_site_latitude'][:]
+plon = sf.variables['prof_site_longitude'][:]
+pagl = sf.variables['prof_height_100m_AGL'][:]
 
 # Get data from all files
 y = mf.variables['prof_time_year'][:]
@@ -71,6 +71,32 @@ mmean = mf.variables['prof_dar50_Merid_mean'][:]
 # Read in 920 MHz data from all files
 raindbz = mf.variables['prof_dar920_vert_zdb'][:]
 
-# Apply the bad data flag to turn off data we don't want
+# Create arrays with bad data turned off (masked, using the value of "bf")
 omean2 = np.ma.masked_values(omean,bf)
+raindbz2 = np.ma.masked_values(raindbz,bf)
+
+# Create a new numpy array to hold mask information, initialize to 1
+mask = np.empty_like(omean)
+mask[::] = 1
+
+# Store the height as a 3D variable instead of a single vector
+heights = np.empty_like(omean)
+heights[::] = pagl
+
+# Mark the bad data locations with a 2
+mask[omean == bf] = 2
+
+# Perform test 1 using 920 data
+t1mask1 = (heights > t1z)
+t1mask2 = (~raindbz2.mask)
+combo = np.logical_and(t1mask1,t1mask2)
+mask[combo] = 3
+
+# Now, use the mask array to set any data to "bf" where it's not 1
+good_vals = (mask == 1)
+
+# Why are these a vector and not a 2D thing?
+goodw = omean[good_vals]
+goodp = raindbz[good_vals]
+print(goodw.shape)
 
