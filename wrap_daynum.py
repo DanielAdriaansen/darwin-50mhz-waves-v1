3cd14732b31b8daf26c9912c65b3795c8dc16156
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2016-05-07 12:43:28 -0600

    Initial submission.

---
+++ wrap_daynum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def call(cmd):
+  print(cmd)
+  subprocess.call('%s' % (cmd), shell=True, executable='/bin/csh')
+
+hours = range(0,60,1)
+
+for h in hours:
+  cmd = 'ncl plot_raw_panel.ncl \'daynum=%02d\'' % (int(h))
+  #cmd = 'ncl plot_data_panel.ncl \'daynum=%02d\'' % (int(h))
+  #cmd = 'ncl plot_mask_panel.ncl \'daynum=%02d\'' % (int(h))
+  #cmd = 'ncl plot_prime_panel.ncl \'daynum=%02d\'' % (int(h))
+  #cmd = 'ncl plot_st_input.ncl \'daynum=%02d\'' % (int(h))
+  call(cmd)
