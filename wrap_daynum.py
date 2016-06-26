9f8bbeebdee526f5e7fb33c8b2afd58c0a927176
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2016-06-26 15:53:40 -0600

    Updates to the different calls to scripts.

--- wrap_daynum.py
+++ wrap_daynum.py
@@ -9,9 +9,9 @@ def call(cmd):
 hours = range(0,60,1)
 
 for h in hours:
-  cmd = 'ncl plot_raw_panel.ncl \'daynum=%02d\'' % (int(h))
+  #cmd = 'ncl plot_raw_panel.ncl \'daynum=%02d\'' % (int(h))
   #cmd = 'ncl plot_data_panel.ncl \'daynum=%02d\'' % (int(h))
   #cmd = 'ncl plot_mask_panel.ncl \'daynum=%02d\'' % (int(h))
   #cmd = 'ncl plot_prime_panel.ncl \'daynum=%02d\'' % (int(h))
-  #cmd = 'ncl plot_st_input.ncl \'daynum=%02d\'' % (int(h))
+  cmd = 'ncl plot_st_input.ncl \'daynum=%02d\'' % (int(h))
   call(cmd)
